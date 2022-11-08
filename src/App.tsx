75379807261a5c39769195648fe95cb884e413dd
artem_ <artem_5511@mail.ru>
2022-11-09 02:03:14 +0300

    Initialize project using Create React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Routes, Route, NavLink} from 'react-router-dom';
 import './App.css';
+import {Homepage} from './components/Homepage'
+import {Aboutpage} from './components/Aboutpage'
+import {Blogpage} from './components/Blogpage'
+import {Notfoundpage} from './components/Notfoundpage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App =()=> {
+    return (
+        <div>
+            <header>
+                <NavLink to={"/"}>Home</NavLink>
+                <NavLink to={"/blog"}>Blog</NavLink>
+                <NavLink to={"/about"}>About</NavLink>
+            </header>
+            <Routes>
+                <Route path={"/"} element={<Homepage/>}/>
+                <Route path={"/about"} element={<Aboutpage/>}/>
+                <Route path={"/blog"} element={<Blogpage/>}/>
+                <Route path={"*"} element={<Notfoundpage/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
